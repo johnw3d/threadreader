5ae21266ace9508012f11c8cc88ff1b3022f0eba
johnw3d <johnw>
2014-09-13 13:27:56 -0700

    new layout experiments

--- useful-frags.py
+++ useful-frags.py
@@ -1,3 +1,9 @@
+# set up a py3 virtualenv
 virtualenv --python=/usr/local/Cellar/python3/3.4.1/bin/python3 env3
+
+# to clone a git local repo with .git files
+git archive master | tar -x -C /somewhere/else
+# to export a zipped dump of the repo
+git archive --format zip --output /full/path/to/zipfile.zip master
 
 
